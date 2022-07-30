7287461458d6c55a5dca0ff7c6d0f9374abce5f9
Argelia Skakievich <argeliaska@gmail.com>
2022-07-30 18:37:22 -0500

    feat: add exceptions handler and router

--- app/app.py
+++ app/app.py
@@ -1,10 +1,32 @@
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Request, status
+from fastapi.responses import RedirectResponse, JSONResponse
+from fastapi.exceptions import ValidationError
+from fastapi.exceptions import HTTPException
+from fastapi.encoders import jsonable_encoder
+from app.core.config import settings
+from app.api.api_v1.router import router
 
 app = FastAPI(
     title='Personal Travel Planner',
-    description='This personal travel planner will help you scheduler your travel by showing the weather for departure and return date'
+    description=settings.APP_DESCRIPTION,
+    version='0.1.0',
+    swagger_ui_parameters={'defaultModelsExpandDepth': -1}
 )
+
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(request: Request, exc: ValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({'detail':exc.errors()})
+    )
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=jsonable_encoder({'detail':exc})
+    )
+
 
 @app.get('/health-check')
 def health_check():
@@ -13,3 +35,5 @@ def health_check():
 @app.get('/', response_class=RedirectResponse, include_in_schema=False)
 async def docs():
     return RedirectResponse('/docs')
+
+app.include_router(router, prefix=settings.API_V1)
