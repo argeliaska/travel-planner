e2c79a9cde72a792f15b7e11bb3d18c6630326a2
Argelia Skakievich <argeliaska@gmail.com>
2022-07-30 21:02:05 -0500

    feat: add endpoint to consult forecast

--- app/api/api_v1/router.py
+++ app/api/api_v1/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from app.api.api_v1.routers import geo
+from app.api.api_v1.routers import weather
 
 router = APIRouter()
 
 router.include_router(geo.geo_router, prefix='/geo', tags=['GEO'])
+router.include_router(weather.weather_router, prefix='/weather', tags=['WEATHER'])
--- app/api/api_v1/routers/weather.py
+++ app/api/api_v1/routers/weather.py
@@ -1,4 +1,19 @@
 from fastapi import APIRouter
+from app.schemas.weather_schema import ForecastOutSchema
+from app.services.weather_service import WeatherService
+from datetime import datetime, timedelta
+from app.core.config import settings
 
-weather_router = APIRouter
+weather_router = APIRouter()
+
+tomorrow = datetime.now()
+tomorrow += timedelta(days=1) 
+
+@weather_router.get('/onecall', summary="Gets the current weather info and forecast", 
+                    ) # response_model=ForecastOutSchema
+def get(lat: float = settings.DEFAULT_DEPARTURE_LAT, 
+        lon: float = settings.DEFAULT_DEPARTURE_LON, 
+        date_da: datetime = tomorrow):
+    return WeatherService.get_forecast(lat, lon, date_da)
+
 
