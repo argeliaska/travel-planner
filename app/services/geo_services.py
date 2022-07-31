cb70ff41698ac3c5748b2da13e2433fcd50f06f9
Argelia Skakievich <argeliaska@gmail.com>
2022-07-30 19:05:40 -0500

    feat: add geo reverse endpoint and not found response

--- app/services/geo_services.py
+++ app/services/geo_services.py
@@ -1,12 +1,14 @@
+from http import HTTPStatus
+from fastapi.exceptions import HTTPException
 from app.utils import get_response
 from app.core.config import settings
-from app.schemas.geo_schema import CityGeoOutSchema
+from app.schemas.geo_schema import CityGeoOutSchema, CityGeoRevOutSchema
 
 import logging
 
 class GeoService():
     @staticmethod
-    def get(q: str=None):
+    def get_info(q: str=None):
         
         results = []
         
@@ -16,7 +18,33 @@ class GeoService():
             params.update(settings.OPEN_WEATHER_APIKEY_PARAM)
             params.update(settings.CITIES_GEO_INFO_LIMIT)
             resp = get_response(url, params)
-
-            results = [CityGeoOutSchema(**city) for city in resp]
+            
+            if resp:
+                results = [CityGeoOutSchema(**city) for city in resp]
+            else:
+                raise HTTPException(
+                    status_code=HTTPStatus.NOT_FOUND,
+                    detail="City not found"
+                )
 
         return results
+
+    @staticmethod
+    def get_info_rev(lat: float=None, lon: float=None):
+        
+        result = None
+
+        url = f'{settings.GEO_REV_API_URL}'
+        params = {'lat': lat, 'lon': lon}
+        params.update(settings.OPEN_WEATHER_APIKEY_PARAM)
+        resp = get_response(url, params)
+        if resp:
+            result = CityGeoRevOutSchema(**resp)
+        else:
+            raise HTTPException(
+                status_code=HTTPStatus.NOT_FOUND,
+                detail="City not found, incorrect latitud or longitud"
+            )
+
+        return result
+    
